e15b860ca2bb91ec2bd6caa199677b5f708d953f
katreddyvenkateswaraReddy <63310358+katreddyvenkateswaraReddy@users.noreply.github.com>
2024-01-27 12:15:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,42 +1,33 @@
 function convertToRoman(num) {
-  	const obj = {
-      0:['M',1000], 
-      1:['D', 500], 
-      2:['C', 100], 
-      3:['L', 50], 
-      4:['X', 10], 
-      5:['V', 5], 
-      6:['I', 1]
-    };
-  let result = '';
+  const romanNumerals = [
+    { value: 1000, symbol: 'M' },
+    { value: 900, symbol: 'CM' },
+    { value: 500, symbol: 'D' },
+    { value: 400, symbol: 'CD' },
+    { value: 100, symbol: 'C' },
+    { value: 90, symbol: 'XC' },
+    { value: 50, symbol: 'L' },
+    { value: 40, symbol: 'XL' },
+    { value: 10, symbol: 'X' },
+    { value: 9, symbol: 'IX' },
+    { value: 5, symbol: 'V' },
+    { value: 4, symbol: 'IV' },
+    { value: 1, symbol: 'I' }
+  ];
 
-  // Iterate through the keys of the obj object
-  for (let key in obj) {
-    // Get the Roman numeral and its corresponding value
-    const [romanNumeral, value] = obj[key];
+  let romanNumeral = '';
 
-    // Calculate the number of times the current value fits into num
-    const count = Math.floor(num / value);
-
-    // Append the Roman numeral to the result string count times
-    result += romanNumeral.repeat(count);
-
-    // Update num to remove the counted value
-    num %= value;
+  for (let i = 0; i < romanNumerals.length; i++) {
+    while (num >= romanNumerals[i].value) {
+      romanNumeral += romanNumerals[i].symbol;
+      num -= romanNumerals[i].value;
+    }
   }
 
-  return result;
-
-  //your code here
-
+  return romanNumeral;
 }
-// You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
 
-console.log(convertToRoman(36));
-
-
-
-
-
-// do not edit below this line
-module.exports = convertToRoman
+// Example usage:
+const number = 3549;
+const romanRepresentation = convertToRoman(number);
+console.log(romanRepresentation); // Output: "MMMDXLIX"
